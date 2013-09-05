40646154a6a9b6828c1909d86862a07a7ca2cf3e
localdisk <smoochynet@gmail.com>
2013-09-05 13:46:49 +0900

    Bootstrap V3 対応

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,5 @@
 
 Route::get('/', function()
 {
-	return View::make('home');
+    return View::make('home');
 });
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -1,93 +1,86 @@
 <!DOCTYPE html>
 <html lang="ja">
-	<head>
-		<!-- Basic Page Needs
-		================================================== -->
-		<meta charset="utf-8" />
-		<title>
-			@section('title')
-			Laravel 4 - Bootstrap App
-			@show
-		</title>
-		<meta name="keywords" content="your, awesome, keywords, here" />
-		<meta name="author" content="localdisk" />
-		<meta name="description" content="Laravel 4 Bootstrap App Japanese Edition with Twitter Bootstrap and Generators, IDE Helper" />
+    <head>
+        <!-- Basic Page Needs
+        ================================================== -->
+        <meta charset="utf-8" />
+        <title>
+            @section('title')
+            Laravel 4 - Bootstrap App
+            @show
+        </title>
+        <meta name="keywords" content="your, awesome, keywords, here" />
+        <meta name="author" content="localdisk" />
+        <meta name="description" content="Laravel 4 Bootstrap App Japanese Edition with Twitter Bootstrap and Generators, IDE Helper" />
 
-		<!-- Mobile Specific Metas
-		================================================== -->
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Mobile Specific Metas
+        ================================================== -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-		<!-- CSS
-		================================================== -->
+        <!-- CSS
+        ================================================== -->
         <?= HTML::style('css/bootstrap.css') ?>
-        <?= HTML::style('css/bootstrap-responsive.css') ?>
 
-		<style>
-		@section('styles')
-			body {
-				padding-top: 60px;
-			}
-		@show
-		</style>
+        <style>
+            @section('styles')
+            body {
+                min-height: 2000px;
+                padding-top: 70px;
+            }
+            @show
+        </style>
 
-		<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-		<!--[if lt IE 9]>
-		<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+        <!-- Favicons
+        ================================================== -->
+    </head>
 
-		<!-- Favicons
-		================================================== -->
-	</head>
+    <body>
+        <!-- Navbar -->
+        <!-- Fixed navbar -->
+        <div class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li <?= (Request::is('/') ? 'class="active"' : '') ?>><a href="<?= URL::to('') ?>">Home</a></li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        @if (Auth::check())
+                        <li>Logged in as <?= Auth::user()->username ?></li>
+                        <li></li>
+                        <li  <?= (Request::is('account') ? 'class="active"' : '') ?>><a href="<?= URL::to('user') ?>">Account</a></li>
+                        <li><a href="<?= URL::to('user/logout') ?>">Logout</a></li>
+                        @else
+                        <li <?= (Request::is('user/login') ? 'class="active"' : '') ?>><a href="<?= URL::to('user/login') ?>">Login</a></li>
+                        <li <?= (Request::is('user/register') ? 'class="active"' : '') ?>><a href="<?= URL::to('user/register') ?>">Sign Up</a></li>
+                        @endif
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </div>
+        <!-- ./ navbar -->
+        <!-- Container -->
+        <div class="container">
+            <!-- notifications -->
+            @include('notifications')
+            <!-- ./ notifications -->
 
-	<body>
-		<!-- Navbar -->
-		<div class="navbar navbar-inverse navbar-fixed-top">
-			<div class="navbar-inner">
-				<div class="container">
-					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</a>
+            <!-- Content -->
+            @yield('content')
+            <!-- ./ content -->
+        </div>
+        <!-- ./ container -->
 
-					<div class="nav-collapse collapse">
-						<ul class="nav">
-							<li <?= (Request::is('/') ? 'class="active"' : '') ?>><a href="<?= URL::to('') ?>">Home</a></li>
-						</ul>
-
-			                        <ul class="nav pull-right">
-			                            @if (Auth::check())
-			                            <li class="navbar-text">Logged in as <?= Auth::user()->username ?></li>
-			                            <li class="divider-vertical"></li>
-			                            <li  <?= (Request::is('account') ? 'class="active"' : '') ?>><a href="<?= URL::to('user') ?>">Account</a></li>
-			                            <li><a href="<?= URL::to('user/logout') ?>">Logout</a></li>
-			                            @else
-			                            <li <?= (Request::is('user/login') ? 'class="active"' : '') ?>><a href="<?= URL::to('user/login') ?>">Login</a></li>
-			                            <li <?= (Request::is('user/register') ? 'class="active"' : '') ?>><a href="<?= URL::to('user/register') ?>">Sign Up</a></li>
-			                            @endif
-			                        </ul>
-					</div>
-					<!-- ./ nav-collapse -->
-				</div>
-			</div>
-		</div>
-		<!-- ./ navbar -->
-
-		<!-- Container -->
-		<div class="container">
-			<!-- Notifications -->
-			@include('notifications')
-			<!-- ./ notifications -->
-
-			<!-- Content -->
-			@yield('content')
-			<!-- ./ content -->
-		</div>
-		<!-- ./ container -->
-
-		<!-- Javascripts
-		================================================== -->
+        <!-- Javascripts
+        ================================================== -->
         <?= HTML::script('js/jquery-1.10.2.min.js') ?>
-        <?= HTML::script('js/bootstrap.min.js') ?>
-	</body>
+        <?= HTML::script('js/bootstrap.js') ?>
+    </body>
 </html>
